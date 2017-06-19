5c9a4525ce60757a180ffa6416f2ae6b339c345e
Dan Aloni <alonid@gmail.com>
2017-06-19 12:59:08 +0300

    This day's updates

--- zshrc.sh
+++ zshrc.sh
@@ -40,9 +40,9 @@ alias gir='tig refs'
 alias gamd='git amendNDA'
 alias gre='grep'
 alias godo='git todo'
-alias hgre='grep-hist'
 alias a='cat ${ZSH_ROOT}/zshrc.sh | grep ^alias | sort'
 alias v='vim -p'
+alias h='cd ~'
 
 # Typos
 
@@ -64,6 +64,10 @@ stty -ixon
 stty stop undef
 stty start undef
 
+bindkey "^[[5;30001~" accept-line
+bindkey "^[[5;30002~" accept-line
+bindkey "^[[5;30003~" accept-line
+bindkey "^[[5;30005~" accept-line
 
 # Autoload stuff
 
@@ -71,6 +75,7 @@ autoload -U compaudit compinit
 autoload -U colors && colors
 
 # Other options
+
 setopt auto_cd
 setopt multios
 
@@ -104,27 +109,19 @@ set_prompt() {
     # [
     PS1="%{$fg_bold[white]%}[%{$reset_color%}"
 
-    local dh=`print -n "%{\033[1;38;2;0;127;127m%}"`
-    # Path: http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
-    PS1+="${dh}%D{%H:%M}%{$reset_color%} "
+    # Time
 
-    # Path: http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
-    local pwd=${PWD/#$HOME\//}
-    local dirpwd=$(dirname ${pwd})
-    if [[ "$dirpwd" == "." ]] ;then
-	dirpwd=""
-    else
-	dirpwd="$dirpwd/"
-    fi
-    PS1+="%{$fg_bold[magenta]%}%M%{$reset_color$fg[white]%}:${dirpwd}%{$fg_bold[white]%}$(basename ${pwd})%{$reset_color%}"
+    local dh=`print -n "%{\033[1;38;2;0;127;127m%}"`
+    PS1+="${dh}%D{%H:%M}%{$reset_color%}"
 
     # Status Code
     if [[ $LAST_EXIT_CODE == "0" ]] ; then
-	PS1+=" %{$fg_bold[green]%}<%{$reset_color$fg[green]%}%?%{$fg_bold[green]%}>%{$reset_color%}"
+	PS1+=" %{$fg_bold[green]%}<%{$reset_color$fg[green]%}00%{$fg_bold[green]%}>%{$reset_color%}"
     else
-	PS1+=" %{$fg_bold[red]%}<%{$reset_color$fg[red]%}%?%{$fg_bold[red]%}>%{$reset_color%}"
+	PS1+=" %{$fg_bold[red]%}<%{$reset_color$fg[red]%}${(l:2::0:)$(( [##16] $?))}%{$fg_bold[red]%}>%{$reset_color%}"
     fi
 
+    # Git status
     if git rev-parse --is-inside-work-tree 2> /dev/null | grep -q 'true' ; then
 	PS1+=' '
 	PS1+="%{$fg_bold[cyan]%}<%{$reset_color$fg[cyan]%}$(git rev-parse --abbrev-ref HEAD)%{$reset_color%}"
@@ -139,7 +136,7 @@ set_prompt() {
     # Timer: http://stackoverflow.com/questions/2704635/is-there-a-way-to-find-the-running-time-of-the-last-executed-command-in-the-shel
     if [[ $_elapsed[-1] -ne 0 ]]; then
 	PS1+=' '
-	PS1+="%{$fg[magenta]%}$_elapsed[-1]s%{$reset_color%}"
+	PS1+="%{$fg[magenta]%}$_elapsed[-1]%{$reset_color%}"
     fi
 
     # PID
@@ -155,6 +152,19 @@ set_prompt() {
 	PS1+=' '
 	PS1+="%{$fg_bold[red]%}SUDO%{$reset_color%}"
     fi
+
+    # Path: http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
+
+    local pwd=${PWD/#$HOME\//}
+    local dirpwd=$(dirname ${pwd})
+    if [[ "$dirpwd" == "." ]] ;then
+	dirpwd=""
+    else
+	dirpwd="$dirpwd/"
+    fi
+    PS1+=" %{$fg_bold[magenta]%}%M%{$reset_color$fg[white]%}:${dirpwd}%{$fg_bold[white]%}$(basename ${pwd})%{$reset_color%}"
+
+    # End
 
     PS1+="%{$fg_bold[white]%}]%{$reset_color%}$ "
 }
